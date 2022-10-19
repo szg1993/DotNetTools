f21d890258b3c8af3501657ff7621ac8b9461457
Gábor Sztárcsevics <szg1993@gmail.com>
2022-10-19 18:37:46 +0200

    GetException method added.

--- ExGuard/Demo/Program.cs
+++ ExGuard/Demo/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+try
+{
+
+}
+catch (Exception)
+{
+
+	throw;
+}
---
+++ ExGuard/ExGuard/ExGuard.cs
@@ -0,0 +1,8 @@
+﻿namespace ExGuard
+{
+    public static class ExGuard
+    {
+        private static TException GetException<TException>(string message) where TException : Exception, new()
+            => (TException)Activator.CreateInstance(typeof(TException), message);
+    }
+}
