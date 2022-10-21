06420d0efe0a883d85233f7251656b61ad62ca07
Sztárcsevics Gábor <Gabor.Sztarcsevics@sagemcom.hu>
2022-10-21 10:37:17 +0200

    LesserThan added.

--- ExGuard/ExGuard/Helpers/LesserThan.cs
+++ ExGuard/ExGuard/Helpers/LesserThan.cs
@@ -3,47 +3,47 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace ExGuard.Helpers
 {
-    public static class GreaterThan
+    public static class LesserThan
     {
-        public static int ThrowIfGreater([NotNull] this int validatable, int limit)
+        public static int ThrowIfLesser([NotNull] this int validatable, int limit)
             => ValidateInt(validatable, limit: limit, message: null, exceptionType: null);
 
-        public static int ThrowIfGreater([NotNull] this int validatable, int limit, string message)
+        public static int ThrowIfLesser([NotNull] this int validatable, int limit, string message)
             => ValidateInt(validatable, limit: limit, message: message, exceptionType: null);
 
-        public static int ThrowIfGreater([NotNull] this int validatable, int limit, Type exceptionType)
+        public static int ThrowIfLesser([NotNull] this int validatable, int limit, Type exceptionType)
             => ValidateInt(validatable, limit: limit, message: null, exceptionType: exceptionType);
 
-        public static int ThrowIfGreater([NotNull] this int validatable, int limit, string message, Type exceptionType)
+        public static int ThrowIfLesser([NotNull] this int validatable, int limit, string message, Type exceptionType)
             => ValidateInt(validatable, limit: limit, message: message, exceptionType: exceptionType);
 
-        public static double ThrowIfGreater([NotNull] this double validatable, double limit)
+        public static double ThrowIfLesser([NotNull] this double validatable, double limit)
             => ValidateDouble(validatable, limit: limit, message: null, exceptionType: null);
 
-        public static double ThrowIfGreater([NotNull] this double validatable, double limit, string message)
+        public static double ThrowIfLesser([NotNull] this double validatable, double limit, string message)
             => ValidateDouble(validatable, limit: limit, message: message, exceptionType: null);
 
-        public static double ThrowIfGreater([NotNull] this double validatable, double limit, Type exceptionType)
+        public static double ThrowIfLesser([NotNull] this double validatable, double limit, Type exceptionType)
             => ValidateDouble(validatable, limit: limit, message: null, exceptionType: exceptionType);
 
-        public static double ThrowIfGreater([NotNull] this double validatable, double limit, string message, Type exceptionType)
+        public static double ThrowIfLesser([NotNull] this double validatable, double limit, string message, Type exceptionType)
             => ValidateDouble(validatable, limit: limit, message: message, exceptionType: exceptionType);
 
-        public static double ThrowIfGreater([NotNull] this float validatable, float limit)
+        public static double ThrowIfLesser([NotNull] this float validatable, float limit)
             => ValidateFloat(validatable, limit: limit, message: null, exceptionType: null);
 
-        public static double ThrowIfGreater([NotNull] this float validatable, float limit, string message)
+        public static double ThrowIfLesser([NotNull] this float validatable, float limit, string message)
             => ValidateFloat(validatable, limit: limit, message: message, exceptionType: null);
 
-        public static double ThrowIfGreater([NotNull] this float validatable, float limit, Type exceptionType)
+        public static double ThrowIfLesser([NotNull] this float validatable, float limit, Type exceptionType)
             => ValidateFloat(validatable, limit: limit, message: null, exceptionType: exceptionType);
 
-        public static double ThrowIfGreater([NotNull] this float validatable, float limit, string message, Type exceptionType)
+        public static double ThrowIfLesser([NotNull] this float validatable, float limit, string message, Type exceptionType)
             => ValidateFloat(validatable, limit: limit, message: message, exceptionType: exceptionType);
 
         private static int ValidateInt([NotNull] this int validatable, int limit, string message, Type exceptionType)
         {
-            if (validatable <= limit)
+            if (validatable >= limit)
                 return validatable;
 
             throw ExceptionHandler.GetException(message: message, exceptionType: exceptionType);
@@ -51,7 +51,7 @@ namespace ExGuard.Helpers
 
         private static double ValidateDouble([NotNull] this double validatable, double limit, string message, Type exceptionType)
         {
-            if (validatable <= limit)
+            if (validatable >= limit)
                 return validatable;
 
             throw ExceptionHandler.GetException(message: message, exceptionType: exceptionType);
@@ -59,7 +59,7 @@ namespace ExGuard.Helpers
 
         private static double ValidateFloat([NotNull] this float validatable, float limit, string message, Type exceptionType)
         {
-            if (validatable <= limit)
+            if (validatable >= limit)
                 return validatable;
 
             throw ExceptionHandler.GetException(message: message, exceptionType: exceptionType);
